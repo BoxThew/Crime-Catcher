f11a3f0ec55424a73da4294feb9b4c90143a1a13
Rigel <rezerotart@gmail.com>
2026-01-31 19:19:53 -0800

    Skeleton in video dir

--- src/Crime_Catcher/Video/video.py
+++ src/Crime_Catcher/Video/video.py
@@ -1,43 +1,6 @@
 import cv2
 
 
-class Camera:
-
-    def __init__(self):
-        self.cap = cv2.VideoCapture(0)
-        if not self.cam_opened():
-            raise RuntimeError("Could not open camera.")
-
-
-    def cam_opened(self) -> bool:
-        return self.cap.isOpened()
-    
-
-    def activate_cam(self):
-        while True:
-            ret, frame = self.cap.read()
-
-            if not ret:
-                print("Failed to grab frame")
-                break
-
-            cv2.imshow("Security Cam", frame)
-
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-            if cv2.getWindowProperty("Security Cam", cv2.WND_PROP_VISIBLE)< 1:
-                break
-
-        self.cleanup()
-
-    
-    def cleanup(self):
-        self.cap.release()
-        cv2.destroyAllWindows()
-
-
-
 class ThreatDetection:
 
     def __init__(self):
@@ -55,4 +18,60 @@ class ThreatDetection:
 
         pass
 
+
+class Camera:
+
+    def __init__(self):
+        self.threat_detect = ThreatDetection()
+
+        self.cap = cv2.VideoCapture(0)
+        if not self.cam_opened():
+            raise RuntimeError("Could not open camera.")
+
+
+    def cam_opened(self) -> bool:
+        return self.cap.isOpened()
     
+
+
+    def person_detected(self) -> bool:
+        #FIXME
+        return True
+
+
+
+    def active_cam(self):
+        while True:
+            ret, frame = self.cap.read()
+
+            if not ret:
+                print("Failed to grab frame")
+                break
+
+            cv2.imshow("Security Cam", frame)
+
+
+            #skips heavy worj if person not detected
+            if not self.person_detected():
+                continue
+
+            #add threat detection stuff here
+
+
+
+            #exit checkers
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            if cv2.getWindowProperty("Security Cam", cv2.WND_PROP_VISIBLE)< 1:
+                break
+
+        self.cleanup()
+
+    
+    def cleanup(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+
--- src/Crime_Catcher/__main__.py
+++ src/Crime_Catcher/__main__.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     print("This is main!")
 
     cam = video.Camera()
-    cam.activate_cam()
+    cam.active_cam()
